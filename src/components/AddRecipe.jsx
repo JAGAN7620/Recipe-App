14ea103873e59be72cba389ec51b9b3105c51a50
JAGAN7620 <jaganjp7620@gmail.com>
2024-05-07 06:15:33 +0530

    addrecipe

---
+++ src/components/AddRecipe.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const AddRecipe = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Enter title</label>
+                        <input type="text" className="form-control" placeholder='enter recipe title'/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Creator</label>
+                        <input type="text" className="form-control" placeholder='enter the creator'/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Description</label>
+                        <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Type here.."></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Add image</label>
+                        <input type="file" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Select recipe</label>
+                        <select name="" id="" className="form-select">
+                            <option value="select recipe">select recipe</option>
+                            <option value="vegetarian">vegetarian</option>
+                            <option value="non-vegetarian">non-vegetarian</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Ingredients</label>
+                        <input type="text" className="form-control" placeholder='enter ingredients'/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddRecipe
